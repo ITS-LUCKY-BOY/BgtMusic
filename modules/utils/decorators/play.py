afc4f2331b6e43907c8ba6d189efb1eec26ba23c
ITS-LUCKY-BOY <152284180+ITS-LUCKY-BOY@users.noreply.github.com>
2023-12-22 06:47:51 +0530

    Update play.py

--- modules/utils/decorators/play.py
+++ modules/utils/decorators/play.py
@@ -64,7 +64,7 @@ def PlayWrapper(command):
                 if "stream" in message.command:
                     return await message.reply_text(_["str_1"])
                 buttons = botplaylist_markup(_)
-                return await message.reply_text("**🤖 𝐆𝐢𝐯𝐞 🙃 𝐒𝐨𝐦𝐞 💿 𝐐𝐮𝐞𝐫𝐲 😍\n💞 𝐓𝐨 🔊 𝐏𝐥𝐚𝐲 🥀 𝐒𝐨𝐧𝐠 🌷...**\n\n𝐔𝐬𝐚𝐠𝐞 : bgt (Song Name) or /play (Song Name)",
+                return await message.reply_text("**🤖 𝐆𝐢𝐯𝐞 🙃 𝐒𝐨𝐦𝐞 💿 𝐐𝐮𝐞𝐫𝐲 😍\n💞 𝐓𝐨 🔊 𝐏𝐥𝐚𝐲 🥀 𝐒𝐨𝐧𝐠 🌷...**\n\n𝐔𝐬𝐚𝐠𝐞 : Lucky (Song Name) or /play (Song Name)",
                 )
         if message.sender_chat:
             upl = InlineKeyboardMarkup(
